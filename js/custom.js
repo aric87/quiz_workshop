7c91f0705b7903fb2850ff567fc464c4f31f18c2
Aric Johnson <aric87@comcast.net>
2015-10-24 22:40:46 -0400

    finishing touches

--- js/custom.js
+++ js/custom.js
@@ -2,41 +2,46 @@
 //game - # of correct answers, group of questions, current question, number of current question
 //user interactions - start a game, choose an answer, finish the game 
 !function(){
-
+//create the question constructor
 function Question(questionText,correctAnswer,answerOption1,answerOption2,answerOption3,answerOption4){
 	this.questionText = questionText;
 	this.correctAnswer = correctAnswer;
 	this.answerOptions = [answerOption1,answerOption2,answerOption3,answerOption4]
 }
-var question1 = new Question('Which singer joined Mel Gibson in the movie Mad Max: Beyond The Thunderdome?','TINA TURNER','TINA TURNER','Random','Random2','random3');
-var question2 = new Question('Vodka, Galliano and orange juice are used to make which classic cocktail?','HARVEY WALLBANGER','HARVEY WALLBANGER','Random','Random2','random3');
-var question3 = new Question('Which American state is nearest to the former Soviet Union?','HARVEY WALLBANGER','HARVEY WALLBANGER','Random','Random2','random3')
+//create our questions
+var question1 = new Question('Which singer joined Mel Gibson in the movie Mad Max: Beyond The Thunderdome?','Tina Turner','Tina Turner','Larry King','Bionce','Michael Jackson');
+var question2 = new Question('Vodka, Galliano and orange juice are used to make which classic cocktail?','Harvey Wallbanger','Harvey Wallbanger','Long Island Ice Tea','Whiskey Sour','Lamba Squeeze');
+var question3 = new Question('Which American state is nearest to the former Soviet Union?','Alaska','Alaska','Oregon','Hawaii','California')
 var question4 = new Question('In which year did Foinavon win the Grand National?','1967','1967','1966','1968','1969');
-var question5 = new Question('At which battle of 1314 did Robert The Bruce defeat the English forces?','BANNOCKBURN','BANNOCKBURN','Random','Random2','random3')
+var question5 = new Question('At which battle of 1314 did Robert The Bruce defeat the English forces?','Bannockburn','Bannockburn','Isle of Sky','Rowan Tree','Brannagh')
 
-
+//data object that is responsible for handling the data
 var data = {
 	questionsArray:[question1, question2,question3,question4,question5],
 	currentNumber : 0,
 	numberCorrect:0,
-}
-
-var controller = {
-	currentQuestion:data.questionsArray[0],
+	currentQuestion:question1,
 	setCurrentQuestion:function(){
-		this.currentQuestion = data.questionsArray[data.currentNumber]
-	},
+		this.currentQuestion = this.questionsArray[this.currentNumber]
+	}
+}
+//controller that handles the interaction between data and view
+var controller = {
 	iterateQuestion:function(button){
 		var userGuess = button.text();
-		if(userGuess == this.currentQuestion.correctAnswer){
+		if(userGuess == data.currentQuestion.correctAnswer){
 			data.numberCorrect++
 		}
 		data.currentNumber++
-		this.setCurrentQuestion();
-		view.render();
+		if(data.currentNumber == data.questionsArray.length){
+			view.quizDone()
+		} else{
+			data.setCurrentQuestion();
+			view.render();			
+		}
 	}
 }
-
+//view object handles changing the DOM
 var view = {
 	init:function(){
 		this.questionHeading = $('.question');
@@ -49,27 +54,33 @@ var view = {
 		$('button').click(function(){
 			controller.iterateQuestion($(this));
 		})	
+		this.questionTotal.text(data.questionsArray.length);
+		view.render();
 	},
 	render:function(){
-		console.log(controller.currentQuestion);
-		this.questionHeading.text(controller.currentQuestion.questionText);
-		this.button1.text(controller.currentQuestion.answerOptions[0]); 
-		this.button2.text(controller.currentQuestion.answerOptions[1]); 
-		this.button3.text(controller.currentQuestion.answerOptions[2]);  
-		this.button4.text(controller.currentQuestion.answerOptions[3]);
-		this.questionNumber.text(controller.currentQuestionNumber);
+		this.questionHeading.text(data.currentQuestion.questionText);
+		this.button1.text(data.currentQuestion.answerOptions[0]); 
+		this.button2.text(data.currentQuestion.answerOptions[1]); 
+		this.button3.text(data.currentQuestion.answerOptions[2]);  
+		this.button4.text(data.currentQuestion.answerOptions[3]);
+		this.questionNumber.text(data.currentNumber);
+		
+	},
+	quizDone:function(){
+		$('button').hide();
+		this.questionNumber.parent().hide();
+		this.questionHeading.text('Congratulations you got ' + data.numberCorrect + ' correct out of ' + data.questionsArray.length)
 	}
 }
+//allow our game to be accessed through a single global variable 
 window.game = {
 	view : view,
 	data: data,
 	controller : controller
 }
-
-
+//call the init function when the page is ready
 $(document).ready(function(){
 	view.init();
-	view.render();
 });
 
 }()
